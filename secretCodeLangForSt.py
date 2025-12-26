8a700a736a2599ca66036a3dcd0f1ae3bb549e42
shikhasharma-passion <shikha.c.@zhi.org.in>
2025-12-26 13:31:55 +0530

    adding program

---
+++ secretCodeLangForSt.py
@@ -0,0 +1,28 @@
+st = input("Enter Message :")   
+words = st.split(" ")
+coding = False
+
+if(coding):
+    nwords = []
+    for word in words:
+        if(len(word)>=3):
+            r1 = "dsf"
+            r2 = "jkm"
+            stnew = r1+ word[1:] + word[0] + r2
+            nwords.append(stnew)
+        else:
+            nwords.append(word[::-1])
+    print("---Your Encoded String---")
+    print(" ".join(nwords)) 
+
+else:
+    nwords = []
+    for word in words:
+        if(len(word)>=3):
+            stnew = word[3:-3]
+            stnew = stnew[-1] + stnew[:-1]
+            nwords.append(stnew)
+        else:
+            nwords.append(word[::-1])
+    print("---Your Decoded String---")      
+    print(" ".join(nwords)) 
